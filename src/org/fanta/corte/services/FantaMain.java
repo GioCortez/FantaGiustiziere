fc41e2329f17921ee33ad81da7bd1617c856eb07
g.cortesi <g.cortesi@accenture.com>
2023-05-19 14:47:43 +0200

    making application spring-boot-like

---
+++ src/org/fanta/corte/services/FantaMain.java
@@ -0,0 +1,11 @@
+package org.fanta.corte.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantaMain {
+	public static void main(String[] args) {
+		SpringApplication.run(FantaMain.class, args);
+	}
+}
